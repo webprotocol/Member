5389ce8f6feaa0d603bb8a8ccd9f50af227c540f
xxx <xxx@webapp.co.kr>
2015-09-22 10:45:52 +0900

    append view.jsp

--- src/main/java/com/webapp/controller/AjaxController.java
+++ src/main/java/com/webapp/controller/AjaxController.java
@@ -2,9 +2,21 @@ package com.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/ajax")
 public class AjaxController {
+	
+	@RequestMapping(value="/view", method=RequestMethod.GET)
+	public String getView() {
+		
+		return "ajax/view";
+	}
 
+	@RequestMapping(value="/array", method=RequestMethod.GET)
+	public void getArray() {
+		
+		
+	}
 }
